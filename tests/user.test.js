de455e0c701633bd04a2bd4dbb8e0c54f88ff386
Manjik Shrestha <manjik.shrestha@gmail.com>
2020-10-05 14:54:41 +0545

    testing with in-memory jest-mongodb

---
+++ tests/user.test.js
@@ -0,0 +1,39 @@
+
+const dbHandler = require('./db-handler');
+const { User } = require('../modules/user/user.controllers');
+
+
+const userData = {
+  name: 'Manjik',
+  email: 'man@gmail.com',
+  phone: '9841602388',
+  wallet_address: '0x7c0179776BB143a36C9d338F3Fa6149F40BaAc30',
+  agency: '5f770576880d16420eaedefb'
+};
+
+
+describe('User Model Test', () => {
+
+  beforeAll(async () => {
+
+    dbHandler.connect();
+  });
+
+  afterAll(async () => {
+    dbHandler.closeDatabase();
+
+
+  });
+
+  it('create & save user successfully', async () => {
+
+    const savedUser = await User.create(userData);
+
+    expect(savedUser._id).toBeDefined();
+    expect(savedUser.phone).toBe(userData.phone);
+    expect(savedUser.email).toBe(userData.email);
+    expect(savedUser.wallet_address).toBe(userData.wallet_address);
+  });
+
+
+})
