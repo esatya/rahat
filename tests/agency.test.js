7fcac115ec019cd1f5d08c95c872461a157f1f28
Manjik Shrestha <manjik.shrestha@gmail.com>
2020-10-07 10:06:11 +0545

    CRUD testing

--- tests/agency.test.js
+++ tests/agency.test.js
@@ -1,33 +1,62 @@
-const dbHandler = require('./db-handler');
+const { connectDatabase, closeDatabase } = require('./common');
 const { Agency } = require('../modules/agency/agency.controllers');
-const { getExpectedBodyHash } = require('twilio/lib/webhooks/webhooks');
 
 const payload = {
   name: 'Santosh Agency',
+  phone: '98989',
+  email: 'san@gamil.com',
+  address: 'kupondole',
+  token: {
+    name: 'SAN',
+    symbol: 'SN',
+    suppply: '1000000000',
+  },
+  contracts: {
+    rahat: '0xrahat',
+    rahat_admin: '0xrahat-admin',
+    token: '0xtoken',
+  },
 };
 
-beforeAll(() => dbHandler.connect());
-afterAll(() => dbHandler.closeDatabase());
+beforeAll(async () => {
+  await connectDatabase();
+});
+afterAll(async () => {
+  await closeDatabase();
+});
 
 describe('Agency CRUD ', () => {
-  let agencyId;
+  let agency;
   it('can be created correctly', async () => {
-
-    let agency = await Agency.add(payload);
-    agencyId = agency._id;
-    expect(agencyId).toBeDefined();
+    agency = await Agency.add(payload);
+    expect(agency._id).toBeDefined();
     expect(agency.name).toBe(payload.name);
   });
 
   it('can get agency by id', async () => {
+    const data = await Agency.getById(agency._id);
+    expect(data).toBeDefined();
+    expect(typeof data).toBe('object');
+    expect(data.name).toBe(payload.name);
+  });
 
+  it('should approve agency', async () => {
+    const beforeApproved = await Agency.getById(agency._id);
+    await Agency.approve(agency._id);
+    const afterApproved = await Agency.getById(agency._id);
 
-    let agency = await Agency.getById(agencyId);
+    expect(beforeApproved.is_approved).toBe(false);
+    expect(afterApproved.is_approved).toBe(true);
+  });
 
-    expect(agency).toBeDefined();
-    expect(typeof agency).toBe('object');
-    expect(agency.name).toBe(payload.name);
+  it('should update agency details', async () => {
+    const dataToUpdate = {
+      name: 'Manjik',
+      address: 'harisiddhi',
+    };
+    const updatedData = await Agency.update(agency._id, dataToUpdate);
 
-  })
-
+    expect(updatedData.name).toBe(dataToUpdate.name);
+    expect(updatedData.address).toBe(dataToUpdate.address);
+  });
 });
