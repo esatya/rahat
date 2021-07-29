b676c7608bfd86ba5df381811179c5ec58d5e3c3
Binod Chaudhary <thebinod7@gmail.com>
2021-07-29 12:38:06 +0545

    project validator realign

--- modules/project/project.model.js
+++ modules/project/project.model.js
@@ -6,43 +6,47 @@ const { ProjectConstants } = require('../../constants');
 const { ObjectId } = mongoose.Schema;
 
 const schema = {
-  name: {
-    type: String, required: true, trim: true, description: 'Name of project',
-  },
-  status: {
-    type: String,
-    required: true,
-    default: ProjectConstants.status.Draft,
-    enum: Object.values(ProjectConstants.status),
-    description: 'Status of project',
-  },
-  // TODO write cron job to disable project in end_date
-  end_date: { type: Date, description: 'End date of project' },
-  allocations: [{
-    date: { type: Date, required: true, default: Date.now },
-    amount: { type: Number, required: true },
-    txhash: { type: String },
-    success: { type: Boolean, required: true, default: false },
-  }],
-  project_manager: { type: String },
-  location: { type: String, joi: Joi.string().example('kupondole') },
-  financial_institutions: [{ type: ObjectId, ref: 'Institution' }],
-  agency: {
-    type: ObjectId,
-    required: true,
-    ref: 'Agency',
-    description: 'ID of agency that the project belongs to',
-  },
-  ...commonSchema,
+	name: {
+		type: String,
+		required: true,
+		trim: true,
+		description: 'Name of project'
+	},
+	status: {
+		type: String,
+		required: true,
+		default: ProjectConstants.status.Draft,
+		enum: Object.values(ProjectConstants.status),
+		description: 'Status of project'
+	},
+	// TODO write cron job to disable project in end_date
+	end_date: { type: Date, description: 'End date of project' },
+	allocations: [
+		{
+			date: { type: Date, required: true, default: Date.now },
+			amount: { type: Number, required: true },
+			txhash: { type: String },
+			success: { type: Boolean, required: true, default: false }
+		}
+	],
+	project_manager: { type: String }, // Wallet address
+	location: { type: String, joi: Joi.string().example('kupondole') },
+	financial_institutions: [{ type: ObjectId, ref: 'Institution' }],
+	description: String,
+	agency: {
+		type: ObjectId,
+		required: true,
+		ref: 'Agency',
+		description: 'ID of agency that the project belongs to'
+	},
+	...commonSchema
 };
 
-const projectSchema = mongoose.Schema(
-  schema, {
-    collection: 'projects',
-    timestamps: { createdAt: 'created_at', updatedAt: 'updated_at' },
-    toObject: { virtuals: true },
-    toJSON: { virtuals: true },
-  },
-);
+const projectSchema = mongoose.Schema(schema, {
+	collection: 'projects',
+	timestamps: { createdAt: 'created_at', updatedAt: 'updated_at' },
+	toObject: { virtuals: true },
+	toJSON: { virtuals: true }
+});
 
 module.exports = mongoose.model('Project', projectSchema);
