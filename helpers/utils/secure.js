30451feb4aa097390deb7c18e1eb8ee3262a0e01
Manjik Shrestha <manjik.shrestha@gmail.com>
2021-08-04 17:07:37 +0545

    updated secure api with auth-signature

--- helpers/utils/secure.js
+++ helpers/utils/secure.js
@@ -1,26 +1,56 @@
-const { User } = require('../../modules/user/user.controllers');
+const config = require('config');
+const ethers = require('ethers');
+const { User, getByWalletAddress } = require('../../modules/user/user.controllers');
+const { Role } = require('../../modules/user/role.controllers');
 
 const checkPermissions = (user_perm, access_perm) => user_perm.some((v) => access_perm.indexOf(v) !== -1);
+
+const GetAddressFromSignature = (signatureWithData) => {
+  if (!signatureWithData) throw Error("Must send 'auth-signature'");
+
+  const [data, signature] = signatureWithData.split('.');
+  const dateDiff = (Date.now() - parseInt(data, 10)) / 1000;
+
+  if (dateDiff > config.get('app.signatureValidityMinutes')) throw Error('Signature has expired.');
+  return ethers.utils.verifyMessage(data, signature);
+};
 
 const Secure = async (reoutePermissions, req) => {
   if (reoutePermissions.length === 0) return true;
 
   const token = req.query.access_token || req.headers.access_token;
-  if (!token) throw Error('No access token was sent');
+  const authSignature = req.query.authSignature || req.headers.authSignature;
 
-  try {
-    const t = await User.validateToken(token);
-    req.currentUser = t.user;
-    req.currentUserId = t.user._id;
-    if (req.payload) {
-      req.payload.updated_by = t.user._id;
-      req.payload.currentUser = t.user;
-      if (req.method.toLowerCase() === 'post') req.payload.created_by = t.user._id;
+  let user;
+  let permissions;
+  const updateUserReq = async () => {
+    try {
+      if (user.roles) permissions = await Role.calculatePermissions(user.roles);
+      req.currentUser = user;
+      req.currentUserId = user._id;
+      if (req.payload) {
+        req.payload.updated_by = user._id;
+        req.payload.currentUser = user;
+        if (req.method.toLowerCase() === 'post') req.payload.created_by = user._id;
+      }
+      const userPerms = permissions || [];
+      return checkPermissions(userPerms, reoutePermissions);
+    } catch (e) {
+      return false;
     }
-    const userPerms = t.permissions || [];
-    return checkPermissions(userPerms, reoutePermissions);
-  } catch (e) {
-    return false;
+  };
+  if (!token && !authSignature) throw Error('No access token or authSignatures was sent');
+
+  if (authSignature) {
+    const wallet_address = GetAddressFromSignature(authSignature);
+
+    user = await getByWalletAddress(wallet_address);
+    return updateUserReq();
+  }
+  if (token) {
+    const tokenData = await User.validateToken(token);
+    user = tokenData.user;
+    return updateUserReq();
   }
 };
 
