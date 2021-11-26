92a68ff2f6db7b21d88718146b75fcc8f6a4edbe
Raktim Shrestha <raktim@rumsan.com>
2021-11-26 14:04:03 +0545

    mongodb Memory test server for db mock

--- jest.config.js
+++ jest.config.js
@@ -84,7 +84,7 @@ module.exports = {
 	// notifyMode: "failure-change",
 
 	// A preset that is used as a base for Jest's configuration
-	// preset: null,
+	preset: '@shelf/jest-mongodb',
 
 	// Run tests from one or more projects
 	// projects: null,
@@ -125,7 +125,7 @@ module.exports = {
 	// snapshotSerializers: [],
 
 	// The test environment that will be used for testing
-	testEnvironment: 'node',
+	// testEnvironment: 'node',
 
 	// Options that will be passed to the testEnvironment
 	// testEnvironmentOptions: {},
