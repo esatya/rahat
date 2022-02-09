31cec4f0300e7dc94d0d9f0d98ab2512c8618a99
Rasil <rasilgrt@gmail.com>
2022-02-09 00:32:44 -0500

    fixed some issues and typos in notifiaction generator utils

--- helpers/utils/notification.js
+++ helpers/utils/notification.js
@@ -6,14 +6,14 @@ function generateNotification(type, details) {
       return {
         title: 'New Vendor Registered',
         notificationType: 'Vendor Registered',
-        message: `${details?.name} registered as a Vendor at ${details?.date}`,
+        message: `${details?.name} registered as a Vendor at ${details?.created_at}`,
         redirectUrl: `/vendors/${details?._id}`
       };
     case NOTIFICATION_TYPES.mobilizer_registered:
       return {
         title: 'New Mobilizer Registered',
         notificationType: 'Mobilizer Registered',
-        message: `${details?.name} registered as a Vendor at ${details?.date}`,
+        message: `${details?.name} registered as a Mobilizer at ${details?.created_at}`,
         redirectUrl: `/mobilizers/${details?._id}`
       };
 
