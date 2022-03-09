1a8dd852c36c7b56dd41249b7dd51d30ef09c5af
Manjik Shrestha <myanz.470shrestha@gmail.com>
2022-03-09 19:34:21 +0545

    handle error on sms sent

--- modules/sms/sms.controller.js
+++ modules/sms/sms.controller.js
@@ -4,33 +4,40 @@ const SMS_SERVICE = require('../../helpers/utils/sms');
 
 const Sms = {
   async sendSmsOnPackageIssue(payload) {
-    const {packageName, phone} = payload;
-    const agency = await Agency.getFirst();
-    const sms_payload = {
-      phone,
-      data: {
-        packageName,
-        agency: agency?.name
-      },
-      smsType: SMS_TRIGGER_TYPE.packageIssued
-    };
-    const {data} = await SMS_SERVICE.send_sms(sms_payload);
-    return data;
+    try {
+      const {packageName, phone} = payload;
+      const agency = await Agency.getFirst();
+      const sms_payload = {
+        phone,
+        data: {
+          packageName,
+          agency: agency?.name
+        },
+        smsType: SMS_TRIGGER_TYPE.packageIssued
+      };
+      const {data} = await SMS_SERVICE.send_sms(sms_payload);
+      return data;
+    } catch (e) {
+      return {data: 'unable to send sms', status: false};
+    }
   },
   async sendSmsOnTokenIssue(payload) {
-    const {token, phone} = payload;
-
-    const agency = await Agency.getFirst();
-    const sms_payload = {
-      phone,
-      data: {
-        token,
-        agency: agency?.name
-      },
-      smsType: SMS_TRIGGER_TYPE.tokenIssued
-    };
-    const {data} = await SMS_SERVICE.send_sms(sms_payload);
-    return data;
+    try {
+      const {token, phone} = payload;
+      const agency = await Agency.getFirst();
+      const sms_payload = {
+        phone,
+        data: {
+          token,
+          agency: agency?.name
+        },
+        smsType: SMS_TRIGGER_TYPE.tokenIssued
+      };
+      const {data} = await SMS_SERVICE.send_sms(sms_payload);
+      return data;
+    } catch (e) {
+      return {data: 'unable to send sms', status: false};
+    }
   }
 };
 module.exports = {
