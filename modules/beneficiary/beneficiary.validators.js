f7f0d1ff39b59b879895443050443d7b74843a71
Manjik Shrestha <myanz.470shrestha@gmail.com>
2022-07-01 12:55:46 +0545

    added bank account validator

--- modules/beneficiary/beneficiary.validators.js
+++ modules/beneficiary/beneficiary.validators.js
@@ -21,7 +21,7 @@ module.exports = {
         .allow('')
         .optional()
         .example('5f6b2f815685931cbfe4dad8,2f6b2f815685931cbfe4dad7'),
-      bank: Joi.object({
+      bank_account: Joi.object({
         institution: Joi.string().allow('').optional().example('5f6b2f815685931cbfe4dad8'),
         account_number: Joi.string().allow('').optional().example('12345678'),
         account_name: Joi.string().allow('').optional().example('nepali')
